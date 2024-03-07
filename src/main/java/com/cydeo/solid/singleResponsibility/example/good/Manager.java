bf838c065dde70371e03c559d3d826a288d13b9d
Irfan Tursun <irfan2rsun@gmail.com>
2024-03-07 10:31:15 -0800

    Good example of singleResponsibility

---
+++ src/main/java/com/cydeo/solid/singleResponsibility/example/good/Manager.java
@@ -0,0 +1,14 @@
+package com.cydeo.solid.singleResponsibility.example.good;
+
+public class Manager {
+
+    public static boolean validate(){
+        // Check validation
+        return true;
+    }
+
+    public  static boolean checkSecurity(){
+        // Send to security related part
+        return true;
+    }
+}
---
+++ src/main/java/com/cydeo/solid/singleResponsibility/example/good/Repository.java
@@ -0,0 +1,28 @@
+package com.cydeo.solid.singleResponsibility.example.good;
+
+public class Repository {
+
+
+    public static void create() {
+        // Connect to database
+        // Create user to database
+    }
+
+    public static void read() {
+        // Connect to database
+        // Read user to database
+    }
+
+    public static void update() {
+        // Connect to database
+        // Update user to database
+    }
+
+    public static void delete() {
+        // Connect to database
+        // Delete user to database
+    }
+
+}
+
+
